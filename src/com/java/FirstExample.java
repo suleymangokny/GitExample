9aec681764ebc4d977331e317c1a9954774eeba7
suleymangokny <slyleyman1111@gmail.com>
2022-02-12 13:40:51 -0500

    Just for practice

--- src/com/java/FirstExample.java
+++ src/com/java/FirstExample.java
@@ -4,6 +4,8 @@ public class FirstExample {
 
 	public static void main(String[] args) {
 		// create a main method write main then ctrl + space tab
+		System.out.println("Hello");
+		System.out.println("Test");
 		
 		
 
