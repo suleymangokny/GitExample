e8f866c8b197d0d13f6416cc649c284671c5a6ff
suleymangokny <slyleyman1111@gmail.com>
2022-02-12 13:30:51 -0500

    Created main method

--- src/com/java/FirstExample.java
+++ src/com/java/FirstExample.java
@@ -3,7 +3,9 @@ package com.java;
 public class FirstExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// create a main method write main then ctrl + space tab
+		
+		
 
 	}
 
